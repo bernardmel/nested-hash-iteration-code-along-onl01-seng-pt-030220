1c988b45d010163eead9dd14135ba15a3c40ec37
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-13 22:51:49 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,13 +16,15 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-  
+
     end
   end
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
-        binding.pry
-      end
-    end
-  end
+        if attribute == :favorite_ice_cream_flavors
+             data.delete_if {|ice_cream| ice_cream == "strawberry"}
+           end
+         end
+       end
+     end
